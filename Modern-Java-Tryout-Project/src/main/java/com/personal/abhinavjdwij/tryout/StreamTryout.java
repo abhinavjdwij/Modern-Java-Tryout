7eb044ad0ab977f00b2c9d735cc7b71c4a30d393
Abhinav Jha <abhi28.jha@gmail.com>
2021-03-07 15:28:14 +0530

    Parallel Stream

--- Modern-Java-Tryout-Project/src/main/java/com/personal/abhinavjdwij/tryout/StreamTryout.java
+++ Modern-Java-Tryout-Project/src/main/java/com/personal/abhinavjdwij/tryout/StreamTryout.java
@@ -24,9 +24,20 @@ public class StreamTryout {
         return (list == null) ? null : list.stream().collect(Collectors.toSet());
     }
 
-    public static Map<Integer, Employee> getEmpIdToEmployeeMap(List<Employee> employees) {
+    private static Map<Integer, Employee> getEmpIdToEmployeeMap(List<Employee> employees) {
         return (employees == null) ? null :
                 employees.stream().collect(Collectors.toMap(Employee::getEmpId, employee -> employee));
+    }
+
+    private static void timedSleepOperation(String operationName, int timeout) {
+        System.out.println(operationName + " is a timeout for " + timeout + " milliseconds");
+        System.out.println(operationName + " started at " + System.currentTimeMillis());
+        try {
+            Thread.sleep((long) timeout);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(operationName + " ended at " + System.currentTimeMillis());
     }
 
     /* Exercises */
@@ -51,8 +62,17 @@ public class StreamTryout {
         System.out.println();
     }
 
+    // 3 : Parallel Stream Concurrent Operation
+    public static void exercise3() {
+        System.out.println("Exercise 3");
+        Arrays.asList(1000, 2000, 3000, 4000, 5000).parallelStream().
+                forEach(x -> timedSleepOperation(String.valueOf(x/1000), x));
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         exercise1();
         exercise2();
+        exercise3();
     }
 }
