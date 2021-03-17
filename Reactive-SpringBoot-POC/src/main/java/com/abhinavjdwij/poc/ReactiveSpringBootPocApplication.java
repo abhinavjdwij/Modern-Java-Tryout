96d9c2ee8d2ce54a9f0b9f4a2b0b57b2609282b9
Abhinav Jha <abhi28.jha@gmail.com>
2021-03-17 18:52:16 +0530

    Reactive Spring Boot POC

---
+++ Reactive-SpringBoot-POC/src/main/java/com/abhinavjdwij/poc/ReactiveSpringBootPocApplication.java
@@ -0,0 +1,13 @@
+package com.abhinavjdwij.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSpringBootPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveSpringBootPocApplication.class, args);
+	}
+
+}
